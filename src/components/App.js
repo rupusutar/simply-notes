87fb3ea7311b22ef3697fe8e19c29221c4da3f35
Rupali Sutar <rupusutar@gmail.com>
2020-07-30 22:58:35 +0100

    Add multiple notes from static file using props

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Heading from './Heading'
 import Footer from './Footer'
 import Note from './Note';
+import notes from '../notes';
 
 
 function App() {
   return (
     <div>
       <Heading />
-      <Note />
+      {notes.map(
+        note => <Note key={note.key} title={note.title} content={note.content}/>
+      )}
       <Footer />
     </div>
   );
